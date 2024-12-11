02276ef7226da51d75f73f68f63750b68df62142
SIDDHESH SURYAKANT PAWAR <siddheshpawar785@gmail.com>
2024-12-11 22:33:53 +0530

    Update ArraysBasic.cpp

--- ArraysBasic.cpp
+++ ArraysBasic.cpp
@@ -1,1 +1,30 @@
 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    // int marks;
+    int n;
+    cout<<"Enter the size  of an your aray "<<endl;
+    cin>>n;
+    int marks[n];
+    
+    for ( int i = 0; i <= n-1; i++)
+    {   
+        cout<<"enter "<<i<<" position for number  "<<endl;
+        cin>>marks[i];
+    }
+    
+    cout<<"The arrays elemnts are ";
+     for ( int i = 0; i <= n-1; i++)
+    {   
+        // cout<<"enter "<<i<<" position for number  "<<endl;
+        // cin>>marks[i];
+        cout<<marks[i]<<" ";
+    }
+    
+
+    return 0;
+}
